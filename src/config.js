1f8002a1b248dff61cdccb9cc851da54accab8ea
dede ariansya <dedeariansya1@gmail.com>
2023-02-11 15:27:12 +0800

    add download timeout

--- src/config.js
+++ src/config.js
@@ -4,10 +4,12 @@ const port = Number(process.env.PORT || 4000)
 const maxInputSize = 1024 * 1024 * Number(process.env.MAX_INPUT_IMAGE_SIZE || 5) //MB
 const tmpDir = process.env.TMP_DIR || path.join(__dirname, '../tmp')
 const fileAge = Number(process.env.FILE_AGE || 24) // Hours
+const downloadTimeout = Number(process.env.DOWNLOAD_TIMEOUT || 10) // Seconds
 
 module.exports = {
   port,
   maxInputSize,
   tmpDir,
   fileAge,
+  downloadTimeout,
 }
--- src/downloader.js
+++ src/downloader.js
@@ -1,33 +1,38 @@
 const https = require('https')
-const { maxFileSize } = require('./config')
+const { maxFileSize, downloadTimeout } = require('./config')
 
 const downloader = async (url) => {
   return new Promise(async (resolve, reject) => {
-    const req = https.get('https://' + url, (res) => {
-      const data = []
-      let size = 0
+    const req = https.get(
+      'https://' + url,
+      { timeout: downloadTimeout * 1000 },
+      (res) => {
+        const data = []
+        let size = 0
 
-      res.on('data', (chunk) => {
-        size += chunk.length
-        data.push(chunk)
-        if (size > maxFileSize) {
-          res.destroy(
-            new Error('Resource stream exceeded limit (' + size + ')'),
-          )
-        }
-      })
+        res.on('data', (chunk) => {
+          size += chunk.length
+          data.push(chunk)
+          if (size > maxFileSize) {
+            res.destroy(
+              new Error('Resource stream exceeded limit (' + size + ')'),
+            )
+          }
+        })
 
-      res.on('end', () => {
-        const buffer = Buffer.concat(data)
-        resolve(buffer)
-      })
+        res.on('end', () => {
+          const buffer = Buffer.concat(data)
+          resolve(buffer)
+        })
 
-      res.on('error', reject)
+        res.on('error', reject)
+      },
+    )
+    req.on('timeout', () => {
+      req.destroy()
     })
-
-    req.end()
-
     req.on('error', reject)
+    req.end()
   })
 }
 
