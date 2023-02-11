01857aaec2bffeb0ff0f35d0693e1964189ef833
dede ariansya <dedeariansya1@gmail.com>
2023-02-11 21:12:35 +0800

    setup typescript

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const app = require('./app')
-const { autoRemoveExpired } = require('./imageManager')
-const { port } = require('./config')
-
-http.createServer(app).listen(port, () => {
-  autoRemoveExpired()
-  console.log('server is running on port:' + port)
-})
