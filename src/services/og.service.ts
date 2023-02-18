c3eb8074163fc2dd750153889fa686193f41846c
dede ariansya <dedeariansya1@gmail.com>
2023-02-18 15:09:46 +0800

    update og service

--- src/services/og.service.ts
+++ src/services/og.service.ts
@@ -8,10 +8,10 @@ import { explodeText, formatColor } from '../libs'
  * Types.
  *
  */
-type RequestType = Request<{ name: string }, {}, {}, { [key: string]: string }>
+type RequestType = Request<{ '0': string }, {}, {}, { [key: string]: string }>
 
 type Params = {
-  ext: 'webp' | 'jpeg' | 'jpg' | 'png'
+  ext: 'webp' | 'jpeg' | 'jpg' | 'png' | 'svg'
   text: string
   color: string
   bgColor: string
@@ -23,14 +23,14 @@ type Params = {
  *
  */
 function parseParams(req: RequestType) {
-  let parsed = path.parse(req.params.name)
+  let parsed = path.parse(req.params['0'])
   let ext = (parsed.ext || '.webp').substring(1)
-  let text = parsed.name
-  let color = formatColor(req.query.color, config.colors.dark)
-  let bgColor = formatColor(req.query.bgcolor, config.colors.light)
-  let sign = req.query.sign || ''
+  let text = req.params['0']
+  let color = formatColor(req.query.color || req.query.c, config.colors.dark)
+  let bgColor = formatColor(req.query.bgcolor || req.query.b, config.colors.light)
+  let sign = req.query.sign || req.query.s || ''
 
-  if (!['webp', 'jpeg', 'jpg', 'png'].includes(ext)) ext = 'webp'
+  if (!['webp', 'jpeg', 'jpg', 'png', 'svg'].includes(ext)) ext = 'webp'
 
   return { ext, text, color, bgColor, sign } as Params
 }
@@ -40,7 +40,7 @@ function parseParams(req: RequestType) {
  *
  */
 function createSVG({ text, color, bgColor, sign }: Params) {
-  const lines = explodeText(text, 45).split('\n')
+  const lines = explodeText(text.replaceAll('/n', '\n'), 45).split('\n')
   const space = 55
   let translateY = space * -(lines.length / 2) - 15
 
@@ -56,6 +56,7 @@ function createSVG({ text, color, bgColor, sign }: Params) {
   }
   return `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 630">
+      <title>OG Image</title>
       <rect fill="#${bgColor}" width="1200" height="630" />
       ${text}
     </svg>
@@ -70,14 +71,18 @@ async function handler(req: RequestType, res: Response, next: NextFunction) {
   try {
     const params = parseParams(req)
 
-    const buffer = await sharp(Buffer.from(createSVG(params)))
-      .resize({ width: 1200, height: 630 })
-      .toFormat(params.ext)
-      .toBuffer()
+    let buffer = Buffer.from(createSVG(params))
+    if (params.ext !== 'svg') {
+      buffer = await sharp(buffer).resize({ width: 1200, height: 630 }).toFormat(params.ext).toBuffer()
+    }
+
+    let mime = `image/${params.ext}`
+    if (params.ext === 'jpg') mime = `image/jpeg`
+    if (params.ext === 'svg') mime = `image/svg+xml`
 
     res.header({
       'cache-control': `public, max-age=${config.cacheAge}, must-revalidate`,
-      'content-type': `image/${params.ext == 'jpg' ? 'jpeg' : params.ext}`,
+      'content-type': mime,
       'content-length': buffer.length,
     })
     res.end(buffer, 'binary')
@@ -91,5 +96,5 @@ async function handler(req: RequestType, res: Response, next: NextFunction) {
  *
  */
 export default function (app: Application) {
-  app.get('/og/:name', handler)
+  app.get('/og/*', handler)
 }
